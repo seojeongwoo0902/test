2c233aa444862c164350d67116ebbae54e5977ea
seojengwoo0902 <realteapots@vaultmicro.com>
2022-07-05 17:01:13 +0900

    test

--- test.js
+++ test.js
@@ -1,1 +1,28 @@
-console.log("Hello, World!")
+function solution(s, n) {
+    var answer = '';
+    arr=s.split("")
+
+    function check(item,n){
+        if ((item)===" "){
+            return item.charCodeAt(0)
+        }
+        else if ((item)==="z"||(item)==="Z"){            
+            num= (parseInt(item.charCodeAt(0))-26)+n
+            return num
+        }
+        else{
+            num= item.charCodeAt(0)+n
+            return num
+        }
+
+    }
+    for(let i =0 ; i<arr.length;++i){
+        arr[i]=String.fromCharCode(check(arr[i],n))    
+    }
+    answer=arr.join("")
+    return answer;
+}
+
+console.log(solution("a B z", 4))
+
+
---
+++ test2.js
@@ -0,0 +1,2 @@
+
+console.log(solution("try hello world")); 
